445475ec81df92d5ec62fa522ccdc12a0578cec0
nero1933 <neroigo.1933@gmail.com>
2025-04-05 21:58:58 +0300

    UPDATED: openapi.json and index.html

--- app/db/db.py
+++ app/db/db.py
@@ -1,23 +1,20 @@
 import logging
-from unittest.mock import patch
 
-import pytest
-from sqlalchemy import create_engine, event, Engine
+from sqlalchemy import create_engine, Engine, event
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from core import settings
 
+logging.basicConfig(
+    format="%(message)s",
+    level=logging.INFO)
 
-# logging.basicConfig(
-#     format="%(message)s",
-#     level=logging.INFO)
-#
-#
-# @event.listens_for(Engine, "before_cursor_execute")
-# def log_sql_query(conn, cursor, statement, parameters, context, executemany):
-#     if not settings.DISABLE_SQL_LOGGING:
-#         logging.info(f"SQL --->  {statement}")
+
+@event.listens_for(Engine, "before_cursor_execute")
+def log_sql_query(conn, cursor, statement, parameters, context, executemany):
+    if not settings.DISABLE_SQL_LOGGING:
+        logging.info(f"SQL --->  {statement}")
 
 
 engine = create_engine(settings.DATABASE_URL)
